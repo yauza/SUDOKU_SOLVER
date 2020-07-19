fcd559dcebb4c8c6d947fccaf00e6385c02aebc8
wojkly <wojkly@gmail.com>
2020-07-19 21:39:40 +0200

    add margins

---
+++ src/Sudoku.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import java.awt.*;
+
+public class Sudoku {
+
+    //A convenience method for creating a MaskFormatter.
+    protected static MaskFormatter createFormatter(String s) {
+        MaskFormatter formatter = null;
+        try {
+            formatter = new MaskFormatter(s);
+        } catch (java.text.ParseException exc) {
+            System.err.println("formatter is bad: " + exc.getMessage());
+            System.exit(-1);
+        }
+        return formatter;
+    }
+
+    public static void addComponentsToPane(Container pane){
+
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        for(int i = 0; i < 9 ; i++) {
+            for (int j = 0; j < 9; j++) {
+                JFormattedTextField f = new JFormattedTextField( createFormatter("#") );
+                c.gridy = i;
+                c.gridx = j;
+                c.ipadx = 10;
+                c.insets = new Insets(2,2,0,0);  //padding
+                if (i%3 == 0)
+                    c.insets = new Insets(12,2,0,0);  //padding
+                if (j%3 == 0)
+                    c.insets = new Insets(2,12,0,0);  //padding
+                if (i%3 == 0 && j%3 == 0)
+                    c.insets = new Insets(12,12,0,0);  //padding
+                pane.add(f, c);
+            }
+        }
+
+        JButton button = new JButton("s o l v e");
+        c.gridx = 2;
+        c.gridy = 10;
+        c.gridwidth = 5;
+        c.weighty = 1.0;
+        pane.add(button,c);
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("s u d o k u");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.setSize(300,300);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
