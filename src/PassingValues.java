42ff459b810340e976f5942923634583091c7fa1
yauza <61622378+yauza@users.noreply.github.com>
2020-07-21 12:32:37 +0200

    minor changes

--- src/PassingValues.java
+++ src/PassingValues.java
@@ -3,15 +3,23 @@ import javax.swing.*;
 public class PassingValues{
     int[][] array = new int[9][9];
 
+    public static Integer tryParse(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
     public void gettingValues(JFormattedTextField[] fields){
 
         for (int i = 0; i < 9; i++){
             for (int j = 0; j < 9; j++){
                 int temp;
                 if (fields[(9*i + j)].getText() == null){
-                    temp = Integer.parseInt(fields[(9*i + j)].getText());
-                } else {
                     temp = 0;
+                } else {
+                    temp = tryParse(fields[(9*i + j)].getText());
                 }
                 array[i][j] = temp;
             }
